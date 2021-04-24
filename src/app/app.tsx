4c81d651aec7ccf5f76b9569085717c8fa6b0ee3
Andres Rios <rianby64@gmail.com>
2021-04-24 19:48:30 +0300

    Initial simple version

---
+++ src/app/app.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import { render } from 'react-dom';
+import { MyExampleOL } from './ol_example';
+
+render(
+    (<MyExampleOL></MyExampleOL>),
+    document.getElementById("root")
+);
