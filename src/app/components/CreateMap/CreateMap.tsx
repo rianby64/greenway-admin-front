cc4b771c6faba40064787b2fc96e7ca28be6ee80
LexaMart <lexamart1708@gmail.com>
2022-01-01 15:54:50 +0300

    feat: add user version template

--- src/app/components/CreateMap/CreateMap.tsx
+++ src/app/components/CreateMap/CreateMap.tsx
@@ -1,49 +1,49 @@
 import React from "react";
 import { MapContainer } from "react-leaflet";
 import { useTypedSelector } from "../../../redux/useTypedSelector.hook";
-import { DropDown } from "../DropDawn/DropDawn";
+// import { DropDown } from "../DropDawn/DropDawn";
 import { MapControl } from "../MapControl/MapControl";
 import { DescriptionComponent } from "../PointDescriptionComponent/Description";
 import { SaveRoute } from "../SaveRoute/saveRoute";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { setDefaultState } from "../../../redux/useEditRouteReducer";
-import { getAllRoutes } from "../../../axios/requests";
+// import { getAllRoutes } from "../../../axios/requests";
 
 export const CreateMap: React.FunctionComponent = () => {
   const { currentFeature } = useTypedSelector((store) => store.route);
   const [saveRouteMenu, setSaveRouteMenu] = React.useState<boolean>(false);
-  const [fetchedRoutes, setFetchedRoutes] = React.useState<Array<any>>([]);
-  const [fetchedVerified, setFetchedVerified] = React.useState<Array<any>>([]);
-  const [fetchedNotVerified, setFetchedNotVerified] = React.useState<Array<any>>([]);
+  // const [fetchedRoutes, setFetchedRoutes] = React.useState<Array<any>>([]);
+  // const [fetchedVerified, setFetchedVerified] = React.useState<Array<any>>([]);
+  // const [fetchedNotVerified, setFetchedNotVerified] = React.useState<Array<any>>([]);
 
 
   const dispatch = useDispatch();
 
-  const filterNotVerified = (array): void => {
-    setFetchedNotVerified(array.filter((el) => !el.approved));
-    console.log(fetchedNotVerified, "notverif");
-    console.log(fetchedRoutes, "notverif");
+  // const filterNotVerified = (array): void => {
+  //   setFetchedNotVerified(array.filter((el) => !el.approved));
+  //   console.log(fetchedNotVerified, "notverif");
+  //   console.log(fetchedRoutes, "notverif");
 
 
-  };
+  // };
 
-  const filterVerified = (array): void => {
-    setFetchedVerified(array.filter((el) => el.approved));
-    console.log(fetchedVerified, "verif");
+  // const filterVerified = (array): void => {
+  //   setFetchedVerified(array.filter((el) => el.approved));
+  //   console.log(fetchedVerified, "verif");
     
-  };
+  // };
 
-  const fetchAllRoutes = async () => {
-    const fetchedData = await getAllRoutes();
-    setFetchedRoutes(fetchedData);
-    filterVerified(fetchedData);
-    filterNotVerified(fetchedData);
-  };
+  // const fetchAllRoutes = async () => {
+  //   const fetchedData = await getAllRoutes();
+  //   setFetchedRoutes(fetchedData);
+  //   filterVerified(fetchedData);
+  //   filterNotVerified(fetchedData);
+  // };
 
   useEffect(() => {
     dispatch(setDefaultState());
-    fetchAllRoutes();
+    // fetchAllRoutes();
   }, []);
 
   return (
@@ -65,8 +65,8 @@ export const CreateMap: React.FunctionComponent = () => {
       >
         СОХРАНИТЬ
       </button>
-      <DropDown fetchedRoutes={fetchedVerified} title={"Проверенные маршруты"} left={"60%"} />
-      <DropDown fetchedRoutes={fetchedNotVerified} title={"Не проверенные маршруты"} left={"40%"} />
+      {/* <DropDown fetchedRoutes={fetchedVerified} title={"Проверенные маршруты"} left={"60%"} />
+      <DropDown fetchedRoutes={fetchedNotVerified} title={"Не проверенные маршруты"} left={"40%"} /> */}
       <SaveRoute
         isEditing={false}
         isShawn={saveRouteMenu}
--- src/app/components/SaveRoute/components/SaveRoute-switches.tsx
+++ src/app/components/SaveRoute/components/SaveRoute-switches.tsx
@@ -1,58 +1,110 @@
-import React from 'react';
-import { Switch } from 'react-materialize';
-import { SaveSwitches } from '../../../../types/Types';
+import React from "react";
+import { Switch } from "react-materialize";
+import { SaveSwitches } from "../../../../types/Types";
 
-export const SaveRouteSwitches: React.FunctionComponent<SaveSwitches> = ({ saveForm, setSaveForm }) => {
+export const SaveRouteSwitches: React.FunctionComponent<SaveSwitches> = ({
+  saveForm,
+  setSaveForm,
+}) => {
   const switchHandler = (e) => {
     switch (e.target.id) {
-      case 'children':
+      case "children":
         setSaveForm({
           ...saveForm,
-          children: !saveForm.children
+          children: !saveForm.children,
         });
         break;
-      case 'wheelChair':
+      case "wheelChair":
         setSaveForm({
           ...saveForm,
-          wheelChair: !saveForm.wheelChair
+          wheelChair: !saveForm.wheelChair,
         });
         break;
-      case 'visuallyImpaired':
+      case "visuallyImpaired":
         setSaveForm({
           ...saveForm,
-          visuallyImpaired: !saveForm.visuallyImpaired
+          visuallyImpaired: !saveForm.visuallyImpaired,
         });
         break;
-      case 'approved':
+      case "approved":
         setSaveForm({
           ...saveForm,
-          approved: !saveForm.approved
-        })
+          approved: !saveForm.approved,
+        });
         break;
-      case 'animals':
+      case "animals":
         setSaveForm({
           ...saveForm,
-          animals: !saveForm.animals
-        })
+          animals: !saveForm.animals,
+        });
         break;
       default:
         break;
     }
-  }
+  };
 
   return (
-    <div className='switches_handler' style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '95%' }}>
-      <div className='switches_pair' style={{ display: 'flex', flexFlow: 'column', justifyContent: 'center', alignItems: 'center' }}>
-        <Switch id='animals' checked={saveForm.animals} onChange={switchHandler} onLabel='С животными' offLabel='Без животных' />
-        <Switch id='children' checked={saveForm.children} onChange={switchHandler} onLabel='С детьми' offLabel='Без детей' />
+    <div
+      className="switches_handler"
+      style={{
+        display: "flex",
+        justifyContent: "space-around",
+        alignItems: "center",
+        width: "95%",
+        marginBottom: "10px",
+      }}
+    >
+      <div
+        className="switches_pair"
+        style={{
+          display: "flex",
+          flexFlow: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Switch
+          id="animals"
+          checked={saveForm.animals}
+          onChange={switchHandler}
+          onLabel="С животными"
+          offLabel="Без животных"
+        />
+        <Switch
+          id="children"
+          checked={saveForm.children}
+          onChange={switchHandler}
+          onLabel="С детьми"
+          offLabel="Без детей"
+        />
       </div>
-      <div className='switches_pair' style={{ display: 'flex', flexFlow: 'column', justifyContent: 'center', alignItems: 'center' }}>
-        <Switch id='wheelChair' checked={saveForm.wheelChair} onChange={switchHandler} onLabel='Подходит людям на коляске' offLabel='Не подходит людям на коляске' />
-        <Switch id='visuallyImpaired' checked={saveForm.visuallyImpaired} onChange={switchHandler} onLabel='Доступно с нарушенем зрения' offLabel='Не доступно с нарушенем зрения' />
+      <div
+        className="switches_pair"
+        style={{
+          display: "flex",
+          flexFlow: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Switch
+          id="wheelChair"
+          checked={saveForm.wheelChair}
+          onChange={switchHandler}
+          onLabel="Подходит людям на коляске"
+          offLabel="Не подходит людям на коляске"
+        />
+        <Switch
+          id="visuallyImpaired"
+          checked={saveForm.visuallyImpaired}
+          onChange={switchHandler}
+          onLabel="Доступно с нарушенем зрения"
+          offLabel="Не доступно с нарушенем зрения"
+        />
       </div>
-      <div className='switches_pair' style={{ display: 'flex', flexFlow: 'column', justifyContent: 'center', alignItems: 'center' }}>
+      {/* <div className='switches_pair' style={{ display: 'flex', flexFlow: 'column', justifyContent: 'center', alignItems: 'center' }}>
         <Switch id='approved' checked={saveForm.approved} onChange={switchHandler} onLabel='Проверен' offLabel='Не проверен' />
-      </div>
+      </div> */}
     </div>
-  )
-}
+  );
+};
