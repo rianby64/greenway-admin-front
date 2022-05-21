591cab607f808d7617ce8d5aba9ebe4534253650
LexaMart <lexamart1708@gmail.com>
2022-05-21 20:32:29 +0300

    new selects

---
+++ src/app/components/PointDescriptionComponent/components/styled.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components'
+
+export const CustomSelect = styled.select`
+border-radius: 5px;
+display: initial;
+margin-top: 20px;
+border: 1px solid #777474;
+background: #9c9a9a;
+option {
+    font-size: 16px;
+    :hover {
+        cursor: pointer;
+    }
+}
+`
+
+export const customOption = styled.option`
+
+`
+
+export const SelectContainer = styled.div`
+ display: flex;
+ align-items: center;
+ flex-flow column;
+ width: 90%;
+`
