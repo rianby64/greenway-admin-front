736f7a1bcf104cf074be35cbd274f27bac26e129
LexaMart <lexamart1708@gmail.com>
2023-02-11 22:00:23 +0300

    Fix Area checkboxes

--- src/app/components/commonComponents/SaveRoute/components/saveRoute-Area/SaveRoute-Area.tsx
+++ src/app/components/commonComponents/SaveRoute/components/saveRoute-Area/SaveRoute-Area.tsx
@@ -35,34 +35,31 @@ export const AreasCheckboxes: React.FunctionComponent<Checkboxes> = ({ array, la
     }
   }, [array])
 
+
   return (
-
     <>
-      <Styled.styledDiv style={{flexDirection:'column'}}>
-        <Styled.styledUnderTitleLabel className='save-label'>{label} <span style={{color:'#0E7505'}}>*</span></Styled.styledUnderTitleLabel>
+      <Styled.styledDiv style={{ flexDirection: 'column' }}>
+        <Styled.styledUnderTitleLabel className='save-label'>{label} <span style={{ color: '#0E7505' }}>*</span></Styled.styledUnderTitleLabel>
 
-        <div style={{display:'flex', gap:'30px', marginBottom:'30px'}}>
+        <div style={{ display: 'flex', gap: '30px', marginBottom: '30px' }}>
           {array ? array.map((el, ind, array) => {
             return (
-                <CheckboxStyled key={ind} сhecked={el.checked} label={el.title} onChange={() => checkBoxHandler(el, array)}/>
-              //  <Checkbox key={ind} className={'checkboxes'} checked={el.checked} filledIn id={`district_${el.id}`} label={el.title} onChange={() => checkBoxHandler(el, array)} value={el.id} />
+              <CheckboxStyled key={ind} isChecked={el.checked} label={el.title} onChange={() => checkBoxHandler(el, array)} />
             )
           }) : <CheckboxStyled filledIn id={`district_Nodata`} label={`Нет данных`} disabled={true} value={''} />}
         </div>
         {districts.length ?
-            <div style={{marginBottom:'30px'}}>
-              <Styled.styledUnderTitleLabel className='save-label'>{label}</Styled.styledUnderTitleLabel>
-              <div style={{display:'flex', gap:'30px'}}>
-                {districts ? districts.map((el, ind) => {
-                  return (
-                      <CheckboxStyled key={ind} checked={el.checked} label={el.title} onChange={() => districtHandler(el)}/>
-                      //<Checkbox key={ind} className={'checkboxes'} checked={el.checked} filledIn id={`district_${el.id}`} label={el.title} onChange={() => districtHandler(el)} value={el.id} />
-                  )
-                }) : <CheckboxStyled filledIn id={`district_Nodata`} label={`Нет данных`} disabled={true} value={''} />}
-              </div>
-            </div> : null}
+          <div style={{ marginBottom: '30px' }}>
+            <Styled.styledUnderTitleLabel className='save-label'>{label}</Styled.styledUnderTitleLabel>
+            <div style={{ display: 'flex', gap: '30px' }}>
+              {districts ? districts.map((el, ind) => {
+                return (
+                  <CheckboxStyled key={ind} isChecked={el.checked} label={el.title} onChange={() => districtHandler(el)} />
+                )
+              }) : <CheckboxStyled filledIn id={`district_Nodata`} label={`Нет данных`} disabled={true} value={''} />}
+            </div>
+          </div> : null}
       </Styled.styledDiv>
-
     </>
   )
 }
--- src/app/components/commonComponents/checkbox/CheckboxStyled.tsx
+++ src/app/components/commonComponents/checkbox/CheckboxStyled.tsx
@@ -1,18 +1,13 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import * as Styled from './styledCheckbox.js';
-const CheckboxStyled:React.FunctionComponent<any> = (props) => {
-    const changeHandler=()=>{
-        console.log("otrabotal");
+const CheckboxStyled: React.FunctionComponent<any> = (props) => {
+    const changeHandler = () => {
         props.onChange()
     }
-    useEffect(()=>{
-        console.log("123");
-        console.log(props.isChecked, props.label)
-    }, [])
     return (
-        <label style={{alignItems:'center', display:'flex', justifyContent:'center', gap:'12px'}}>
-                <Styled.styledCheckboxInput onClick={changeHandler} checked={props.isChecked} type="checkbox" style={{margin:'0'}} />
-                <Styled.styledCheckboxLabel>{props.label}</Styled.styledCheckboxLabel>
+        <label style={{ alignItems: 'center', display: 'flex', justifyContent: 'center', gap: '12px' }}>
+            <Styled.styledCheckboxInput onChange={changeHandler} checked={props.isChecked} type="checkbox" style={{ margin: '0' }} />
+            <Styled.styledCheckboxLabel>{props.label}</Styled.styledCheckboxLabel>
         </label>
     );
 };
