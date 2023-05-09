319333c7dd3bb583c4b9f2d0990fe9fe5156180a
LexaMart <lexamart1708@gmail.com>
2023-05-09 18:02:35 +0300

    improve of auth logic

--- src/app/hoc/RequireAuth.tsx
+++ src/app/hoc/RequireAuth.tsx
@@ -1,13 +1,13 @@
-import React from "react";
-import { useHistory } from "react-router-dom";
+// import React from "react";
+// import { useHistory } from "react-router-dom";
 
 const RequireAuth = ({ children }) => {
-    const history = useHistory();
+    // const history = useHistory();
     const auth = document.cookie?.indexOf("bearer") !== -1; // FIX AFTER BACK
 
     if (!auth) {
-        history.push('/login');
-        return <></>
+        // history.push('/login');
+        return children
     }
 
     return children;
